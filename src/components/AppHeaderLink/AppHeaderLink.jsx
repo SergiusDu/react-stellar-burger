474c5e3f8c29a81376882869162c20f9da185490
SergiusDu <kanchikov.sergey@gmail.com>
2023-07-27 14:16:20 -0700

    WIP added simple components

---
+++ src/components/AppHeaderLink/AppHeaderLink.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AppHeaderLink({children}) {
+    return (
+        <a>
+            {children}
+        </a>
+    )
+}
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import styles from "./app.module.css";
 import { data } from "../../utils/data";
 import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
+import AppHeader from "../AppHeader/AppHeader";
 
 function App() {
   return (
     <div className={styles.app}>
+        <AppHeader></AppHeader>
       <pre style={{
       	margin: "auto",
       	fontSize: "1.5rem"
       }}>
-      	Измените src/components/app/app.jsx и сохраните для обновления.
+      	А Рита Крыса
       </pre>
         <Button htmlType="button" type="primary" size="small" extraClass="ml-2">
             Нажми на меня
