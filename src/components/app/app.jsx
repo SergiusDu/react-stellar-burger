487e7a4066e77560a59c2098ff3272dd57a6cdaf
Sergei Kanchikov <kanchikov.sergey@gmail.com>
2023-08-15 22:47:38 -0700

    Revert "Завершена начальная фаза разработки проекта "Stellar Burgers":"
    This reverts commit 218b5dfa4a23ead5c8083ff8786a99716942eee6.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,52 +1,24 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import styles from "./app.module.css";
-import AppHeader from "../app-header/app-header";
-import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import BurgerConstructor from "../burger-constructor/burger-constructor";
-import {IngredientProvider} from "../context-providers/ingredient-context";
-import IngredientDetails from "../ingredient-details/ingredient-details";
-import OrderDetails from "../order-details/order-details";
-import OrderContextProvider from "../context-providers/order_details_context";
+import { data } from "../../utils/data";
+import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
+import AppHeader from "../AppHeader/AppHeader";
 
 function App() {
-    const DATA_URL = 'https://norma.nomoreparties.space/api/ingredients';
-    const [ingredients, setIngredients] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-
-    function getIngredients() {
-
-        fetch(DATA_URL).then((response) => response.json())
-            .then(data => {
-                setIngredients(data.data)
-                setIsLoading(false)
-            })
-            .catch(() => {
-                console.log('Не удалось получить данные с сервера.')
-                setIsLoading(true)
-            })
-    }
-
-    useEffect(() => {
-        getIngredients();
-    }, []);
-
-    return (<div className={styles.app}>
-        <AppHeader/>
-        <section className={styles.main_layout}>
-            <IngredientProvider>
-                <OrderContextProvider>
-                    {isLoading ? <div>Loading</div> : (
-                      <>
-                          <BurgerIngredients ingredients={ingredients}/>
-                          <BurgerConstructor ingredients={ingredients}/>
-                      </>
-                     )}
-                    <IngredientDetails/>
-                    <OrderDetails/>
-                </OrderContextProvider>
-            </IngredientProvider>
-        </section>
-    </div>);
+  return (
+    <div className={styles.app}>
+        <AppHeader></AppHeader>
+      <pre style={{
+      	margin: "auto",
+      	fontSize: "1.5rem"
+      }}>
+      	А Рита Крыса
+      </pre>
+        <Button htmlType="button" type="primary" size="small" extraClass="ml-2">
+            Нажми на меня
+        </Button>
+    </div>
+  );
 }
 
 export default App;
