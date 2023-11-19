3b075796772c3360916df3f2218531b2a6570294
SergiusDu <kanchikov.sergey@gmail.com>
2023-11-18 17:55:08 -0800

    fixed a few issues

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/app/app";
-import reportWebVitals from "./reportWebVitals";
-import {Provider} from "react-redux";
-import {store} from "./services/store/store.ts"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/app/app'; // Убедитесь, что App типизирован, если он определен в другом файле
+import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store } from './services/store/store'; // Убедитесь, что store имеет правильные типы
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,10 +12,7 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root') as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
